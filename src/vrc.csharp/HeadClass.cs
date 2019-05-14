f5840faebaa9c7f453d49b67a12f05b47f9081ff
Amadeusz Wieczorek <amwieczo@microsoft.com>
2019-05-14 11:52:08 -0700

    sample class

---
+++ src/vrc.csharp/HeadClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppName
+{
+    class HeadClass
+    {
+        HeadClass a = new HeadClass();
+        HeadClass b = new HeadClass();
+
+        protected void M()
+        {
+            
+        }
+    }
+
+    class DerivedClass : HeadClass
+    {
+        private object iff;
+        private string ints;
+        private object Outt;
+        private object objects;
+
+
+    }
+}
